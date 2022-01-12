d6de3af523202c79dfcfabb033084e0a8321414e
Raashot12 <akannirasheed1996@gmail.com>
2022-01-12 18:28:38 +0100

    master

--- src/components/DashBoard/ProductDetails.jsx
+++ src/components/DashBoard/ProductDetails.jsx
@@ -8,7 +8,7 @@ const ProductDetails = () => {
 
   return (
     <Layout>
-      <div onClick={ () => history.push("/dashboard/properties")} className="property-back-page">
+      <div onClick={ () => history.push( "/dashboard/properties" ) } className="property-back-page">
         <ArrowBackIcon></ArrowBackIcon>
         <p>Back</p>
       </div>
@@ -16,6 +16,7 @@ const ProductDetails = () => {
         <h3>Property Details</h3>
         <Button buttonText="Action" className="btn-action"></Button>
       </div>
+      <MainDetailSinglePage />
     </Layout>
   )
 }
@@ -24,12 +25,25 @@ export default ProductDetails
 
 const MainDetailSinglePage = () => {
   return (
-    <div>
-      <div>
-
-      </div>
-      <div>
-        
+    <div className="container-maindetail-single-page">
+      <div className="container-flex-details">
+        <section>
+          <img src="https://res.cloudinary.com/rashot/image/upload/v1642007572/unsplash_2d4lAQAlbDA_usqkpc.png" alt="" />
+          <div>
+            <h3>Top Court Garden</h3>
+            <p>24, Roland Road, Challenge, Ibadan, Oyo, NG</p>
+            <p>Property Type : Residential</p>
+            <p>Property Sub-Type : Semi- Detached</p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <img src="https://res.cloudinary.com/rashot/image/upload/v1642007563/unsplash_MP0bgaS_d1c_bfy5ak.png" alt="" />
+          </div>
+          <div>
+            <img src="https://res.cloudinary.com/rashot/image/upload/v1642007553/unsplash_L7EwHkq1B2s_ha45pr.png" alt="" />
+          </div>
+        </section>
       </div>
     </div>
   )
