267bad914c4977a892e04329963b832ea0e0e121
Raashot12 <akannirasheed1996@gmail.com>
2022-01-12 18:11:59 +0100

    master

--- src/components/DashBoard/ProductDetails.jsx
+++ src/components/DashBoard/ProductDetails.jsx
@@ -2,20 +2,35 @@ import React from "react"
 import ArrowBackIcon from "../../assets/Icon/ArrowBackIcon"
 import Button from "../../Utilities/Button/Button"
 import Layout from "../Layout/Layout"
-
+import { useHistory } from "react-router-dom"
 const ProductDetails = () => {
+  const history = useHistory()
+
   return (
     <Layout>
-      <div>
+      <div onClick={ () => history.push("/dashboard/properties")} className="property-back-page">
         <ArrowBackIcon></ArrowBackIcon>
         <p>Back</p>
       </div>
-      <div>
+      <div className="property-detail-row-intro">
         <h3>Property Details</h3>
-        <Button buttonText="Action"></Button>
+        <Button buttonText="Action" className="btn-action"></Button>
       </div>
     </Layout>
   )
 }
 
 export default ProductDetails
+
+const MainDetailSinglePage = () => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
